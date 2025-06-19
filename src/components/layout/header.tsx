ca43f9507c6b3e98b6e9b2416e66dd356285207c
Shahbaz Khan <shahbazkhan19111@gmail.com>
2025-06-19 13:56:36 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -86,7 +86,7 @@ export default function Header() {
             </>
           ) : (
             <>
-              <Button asChild variant="outline" size="sm" className="border-primary-foreground text-primary-foreground hover:bg-primary-foreground/20">
+              <Button asChild variant="ghost" size="sm">
                 <Link href="/login">
                   <LogIn className="mr-2 h-4 w-4" />
                   Login
@@ -105,3 +105,4 @@ export default function Header() {
     </header>
   );
 }
+
