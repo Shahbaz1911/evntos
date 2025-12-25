31e3ab102b6daa74b4c673200ebb2e69cf446c19
Shahbaz Khan <shahbazkhan19111@gmail.com>
2025-12-25 17:54:56 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         hostname: 'images.pexels.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
